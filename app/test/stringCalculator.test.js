ca7165177c044577508ecb6ea1765862cb57fe38
JeremyLRYnov <jeremy.lacazerusques@ynov.com>
2022-04-29 13:59:03 +0200

    init commit

--- app/test/stringCalculator.test.js
+++ app/test/stringCalculator.test.js
@@ -10,32 +10,32 @@ describe('stringCalculator', () => {
     });
 
     test("String = '1', return 1", () => {
-        
+
         expect(calculator.add("1")).toBe(1);
     });
 
     test("String = '1,2', return 1", () => {
-        
+
         expect(calculator.add("1,2")).toBe(3);
     });
 
     test("String = '1,2,3,4,5,6,7,8,9', return 1", () => {
-        
+
         expect(calculator.add("1,2,3,4,5,6,7,8,9")).toBe(45);
     });
 
     test("String = '1\n2,3', return 6", () => {
-        
+
         expect(calculator.add("1\n2,3")).toBe(6);
     });
 
     test("String = '//;\n1;2', return 3", () => {
-        
+
         expect(calculator.add("//;\n1;2")).toBe(3);
     });
 
     test("String = '-1', toThrow 'Negatives not allowed'", () => {
-        
+
         expect(() => calculator.add("-1").toThrow('Negatives not allowed. [-1]'));
     });
 });
