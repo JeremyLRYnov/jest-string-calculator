c2bf11ba6866eb0dbdc0d8255abe4cf880f8a895
JeremyLRYnov <jeremy.lacazerusques@ynov.com>
2022-04-29 10:30:00 +0200

    add test for all separator

--- app/test/stringCalculator.test.js
+++ app/test/stringCalculator.test.js
@@ -28,4 +28,9 @@ describe('stringCalculator', () => {
         
         expect(calculator.add("1\n2,3")).toBe(6);
     });
+
+    test("String = '//;\n1;2', return 3", () => {
+        
+        expect(calculator.add("//;\n1;2")).toBe(3);
+    });
 });
